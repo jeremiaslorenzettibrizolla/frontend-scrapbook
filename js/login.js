003d8415749763d238bc4e5483a8e58160e6c740
Jeremias Brizolla <jeremiasbrizolla@gmail.com>
2021-08-06 21:07:53 -0300

    ok

--- js/login.js
+++ js/login.js
@@ -1,6 +1,18 @@
 // axios.defaults.baseURL = "https://api-backend-database.herokuapp.com";
 axios.defaults.baseURL = 'http://localhost:8080';
 
+const signInBtn = document.querySelector("#sign-in-btn");
+const signUpBtn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+signUpBtn.addEventListener("click", () => {
+    container.classList.add("sign-up-mode");
+});
+
+signInBtn.addEventListener("click", () => {
+    container.classList.remove("sign-up-mode");
+});
+
 const username = document.getElementById('register-username');
 const email = document.getElementById('register-email');
 const password = document.getElementById('register-password');
--- js/scrapbook.js
+++ js/scrapbook.js
@@ -3,62 +3,61 @@ const userUID = localStorage.getItem('userUID');
 axios.defaults.baseURL = `http://localhost:8080/users/${userUID}`;
 
 const clearFields = () => {
-  uid.value = '';
-  title.value = '';
-  description.value = '';
+    uid.value = '';
+    title.value = '';
+    description.value = '';
 };
 
 function alertStatus(httpStatus) {
-  if (status === 201) {
-    loadTable();
-    document.getElementById('scrap-post').classList.remove('none');
-    setTimeout(() => {
-      document.getElementById('scrap-post').classList.add('none');
-    }, 2000);
-    return;
-  }
+    if (status === 201) {
+        loadTable();
+        document.getElementById('scrap-post').classList.remove('none');
+        setTimeout(() => {
+            document.getElementById('scrap-post').classList.add('none');
+        }, 2000);
+        return;
+    }
 
-  if (status === 202) {
-    loadTable();
-    document.getElementById('scrap-update').classList.remove('none');
-    setTimeout(() => {
-      document.getElementById('scrap-update').classList.add('none');
-    }, 2000);
-    return;
-  }
+    if (status === 202) {
+        loadTable();
+        document.getElementById('scrap-update').classList.remove('none');
+        setTimeout(() => {
+            document.getElementById('scrap-update').classList.add('none');
+        }, 2000);
+        return;
+    }
 
-  if (status === 204) {
-    loadTable();
-    document.getElementById('scrap-delete').classList.remove('none');
-    setTimeout(() => {
-      document.getElementById('scrap-delete').classList.add('none');
-    }, 2000);
-    return;
-  }
+    if (status === 204) {
+        loadTable();
+        document.getElementById('scrap-delete').classList.remove('none');
+        setTimeout(() => {
+            document.getElementById('scrap-delete').classList.add('none');
+        }, 2000);
+        return;
+    }
 }
 
 async function loadTable() {
-  const scrapbook = document.getElementById('scrapbook');
+    const scrapbook = document.getElementById('scrapbook');
 
-  const { data, status } = await axios.get('/scraps');
+    const { data, status } = await axios.get('/scraps');
 
-  if (data.length === 0) {
-    document.getElementById('scrap-not-found').classList.remove('none');
-    setTimeout(() => {
-      document.getElementById('scrap-not-found').classList.add('none');
-    }, 3000);
-    return;
-  }
+    if (data.length === 0) {
+        document.getElementById('scrap-not-found').classList.remove('none');
+        setTimeout(() => {
+            document.getElementById('scrap-not-found').classList.add('none');
+        }, 3000);
+        return;
+    }
 
-  scrapbook.innerHTML = '';
+    scrapbook.innerHTML = '';
 
-  if (data) {
-    for (let scrap of data) {
-      const line = document.createElement('tr');
-      line.innerHTML = `
-          <th scope="row" class"text-cel" id="${scrap.uid}">${
-        data.indexOf(scrap) + 1
-      }</th>
+    if (data) {
+        for (let scrap of data) {
+            const line = document.createElement('tr');
+            line.innerHTML = `
+          <th scope="row" class"text-cel" id="${scrap.uid}">${data.indexOf(scrap) + 1
+                }</th>
           <td class"text-cel">${scrap.title}</td>
           <td class"text-cel">${scrap.description}</td>
           <td>
@@ -74,68 +73,68 @@ async function loadTable() {
               </div>
           </td>
           `;
-      scrapbook.appendChild(line);
+            scrapbook.appendChild(line);
+        }
     }
-  }
 }
 loadTable();
 
 async function postScrap(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  const uid = document.getElementById('uid');
-  const title = document.getElementById('title');
-  const description = document.getElementById('description');
+    const uid = document.getElementById('uid');
+    const title = document.getElementById('title');
+    const description = document.getElementById('description');
 
-  if (!title.value) {
-    document.getElementById('scrap-error').classList.remove('none');
-    setTimeout(() => {
-      document.getElementById('scrap-error').classList.add('none');
-    }, 2000);
-    return;
-  }
+    if (!title.value) {
+        document.getElementById('scrap-error').classList.remove('none');
+        setTimeout(() => {
+            document.getElementById('scrap-error').classList.add('none');
+        }, 2000);
+        return;
+    }
 
-  const scrap = {
-    title: title.value,
-    description: description.value,
-    userUID: userUID,
-  };
+    const scrap = {
+        title: title.value,
+        description: description.value,
+        userUID: userUID,
+    };
 
-  let response;
+    let response;
 
-  if (!uid.value) {
-    response = await axios.post('/scraps', scrap);
-  } else {
-    response = await axios.put(`/scraps/${uid.value}`, scrap);
-  }
-  alertStatus(response.status);
-  loadTable();
-  clearFields();
+    if (!uid.value) {
+        response = await axios.post('/scraps', scrap);
+    } else {
+        response = await axios.put(`/scraps/${uid.value}`, scrap);
+    }
+    alertStatus(response.status);
+    loadTable();
+    clearFields();
 }
 
 async function updateScrap(event, uid) {
-  event.preventDefault();
+    event.preventDefault();
 
-  const { data } = await axios.get(`/scraps/${uid}`);
-  let scrap = {
-    editUid: data.scrap.uid,
-    editTitle: data.scrap.title,
-    editDescription: data.scrap.description,
-  };
+    const { data } = await axios.get(`/scraps/${uid}`);
+    let scrap = {
+        editUid: data.scrap.uid,
+        editTitle: data.scrap.title,
+        editDescription: data.scrap.description,
+    };
 
-  document.getElementById('uid').value = scrap.editUid;
-  document.getElementById('title').value = scrap.editTitle;
-  document.getElementById('description').value = scrap.editDescription;
+    document.getElementById('uid').value = scrap.editUid;
+    document.getElementById('title').value = scrap.editTitle;
+    document.getElementById('description').value = scrap.editDescription;
 
-  loadTable();
+    loadTable();
 }
 
 async function deleteScrap(event, uid) {
-  event.preventDefault();
+    event.preventDefault();
 
-  const response = await axios.delete(`/scraps/${uid}`);
+    const response = await axios.delete(`/scraps/${uid}`);
 
-  alertStatus(response.status);
+    alertStatus(response.status);
 
-  loadTable();
+    loadTable();
 }
